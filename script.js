43789e6c4f319a84f8bb74a420b514a49a31a48a
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-08-24 14:55:45 +0200

    l

--- script.js
+++ script.js
@@ -29,3 +29,28 @@ document.querySelector(
 
 document.querySelector("article:nth-child(2) img").setAttribute("alt", "my new alt2")
 console.log("My new alt2");
+
+// Opret et nyt article-element
+const newArticle = document.createElement("article");
+
+// Opret et img-element til at tilføje et billede
+const imgElement = document.createElement("img");
+imgElement.src = "img/pose3.png "; // Erstat med den rigtige sti til dit billede
+imgElement.alt = "new alt 3";    // Erstat med en passende beskrivelse
+
+// Opret en h2-element til overskriften
+const h2Element = document.createElement("h2");
+h2Element.textContent = "Den Tredje";
+
+// Opret et p-element til teksten
+const pElement = document.createElement("p");
+pElement.textContent = "Den TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen TredjeDen Tredje.";
+
+// Tilføj billedet, overskriften og teksten til det nye article
+newArticle.appendChild(imgElement);
+newArticle.appendChild(h2Element);
+newArticle.appendChild(pElement);
+
+// Tilføj det nye article til main-sektionen (eller en anden passende forælder)
+const mainSection = document.querySelector("section ");
+mainSection.appendChild(newArticle);

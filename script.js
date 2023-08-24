1504fd3f45ab98459876b93e8cf8fe157413f6b2
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-08-24 10:30:21 +0200

    children

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector("h1").textContent = "Min Side";
+console.log("Min Side");
+
+document.querySelector("figcaption").textContent = "Min SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin Side";
+console.log("Min Side");
+
+document.querySelector("h2").textContent = "Min Side";
+console.log("Min Side");
+
+const info = document.querySelector("#info");
+
+info.lastElementChild.textContent = "Min SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin Side";
+console.log("Min Side");
+
+
+// document.querySelector(".art2").textContent = "Min Side";
+// console.log("Min Side");
+
+// document.querySelector(".fig2").textContent = "Min SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin SideMin Side";
+// console.log("Min Side");
+
